b7fffe5740ff32145de06a650d11f36752582a2b
Julian Grahsl <mail2@grahsl.net>
2011-05-01 18:02:37 +0200

    NestHase baut Nester zusammen.

---
+++ NestHase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using XcoSpaces;
+using XcoSpaces.Collections;
+using XcoSpaces.Exceptions;
+using Eier;
+
+namespace _MalerHase
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            NestHase nh = new NestHase(args[0], new Uri("xco://127.0.0.1:8000"));
+            nh.work();
+        }
+    }
+}
